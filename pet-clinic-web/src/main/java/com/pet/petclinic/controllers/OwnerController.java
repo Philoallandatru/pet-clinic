014e6f367bc63827bbf75c5c305e112e4489e7b7
tianqin <philoallandatruly@outlook.com>
2021-03-08 21:01:57 +0800

    closes #7

---
+++ pet-clinic-web/src/main/java/com/pet/petclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.pet.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/", "/index", "/index.html", ""})
+    String listOwners() {
+        return "owners/index";
+    }
+}
+
