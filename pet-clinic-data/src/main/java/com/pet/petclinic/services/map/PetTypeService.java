87d5a1b217ba0591ea686d4eb653069afdaea4d2
tianqin <philoallandatruly@outlook.com>
2021-03-11 01:40:46 +0800

    closes #22

---
+++ pet-clinic-data/src/main/java/com/pet/petclinic/services/map/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.pet.petclinic.services.map;
+
+import com.pet.petclinic.model.PetType;
+import com.pet.petclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
