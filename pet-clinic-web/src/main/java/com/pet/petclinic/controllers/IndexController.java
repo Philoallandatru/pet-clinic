f1fe33bb3d7209eb81fe87977a1fbe53cf20d5f5
tianqin <philoallandatruly@outlook.com>
2021-03-11 22:22:44 +0800

    closes #26

--- pet-clinic-web/src/main/java/com/pet/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/pet/petclinic/controllers/IndexController.java
@@ -11,4 +11,9 @@ public class IndexController {
     public String index() {
         return  "index";
     }
+
+    @RequestMapping({"/oups",})
+    String ooops() {
+        return "notImplemented";
+    }
 }
--- pet-clinic-web/src/main/java/com/pet/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/pet/petclinic/controllers/OwnerController.java
@@ -20,5 +20,10 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping({"/find",})
+    String findOwners() {
+        return "notImplemented";
+    }
 }
 
