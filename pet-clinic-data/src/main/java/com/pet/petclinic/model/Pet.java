78755b9722f53930a732162addfa76c77c5665b3
tianqin <philoallandatruly@outlook.com>
2021-03-14 02:29:36 +0800

    closes #29

--- pet-clinic-data/src/main/java/com/pet/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/pet/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.pet.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -9,6 +11,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     @ManyToOne
     @JoinColumn(name = "type_id")
